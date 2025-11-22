5728d2fd54f92e5903b6dc5192407d2f426a1fbe
narmeshnigam <nigamnarmesh@gmail.com>
2025-11-22 13:48:55 +0530

    synced with live website code

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Catalog');
+
+// HTTP
+define('HTTP_SERVER', 'https://store.iswift.in/');
+
+// DIR
+define('DIR_OPENCART', '/home/u348991914/domains/store.iswift.in/public_html/');
+define('DIR_APPLICATION', DIR_OPENCART . 'catalog/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_STORAGE', '/home/u348991914/domains/store.iswift.in/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'u348991914_iswiftstore');
+define('DB_PASSWORD', 'iSwiftSt0re');
+define('DB_DATABASE', 'u348991914_iswiftstore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+define('DB_SSL_KEY', '');
+define('DB_SSL_CERT', '');
+define('DB_SSL_CA', '');
